3e33a9c849653fb177907ec4de30f4ef4bc29e85
n1colashka <deckards2505@gmail.com>
2020-04-02 17:49:30 +0600

    interface, slider sections finished (slider not work)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-$(function(){
-    $('.menu-btn').on('click', function(){
+$(function () {
+    $('.menu-btn').on('click', function () {
         $('.left-menu').slideToggle()
     });
 
-    $('.close-btn').on('click', function(){
+    $('.close-btn').on('click', function () {
         $('.left-menu').slideToggle()
     });
+
+    // $('.team-slider').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     fade: true,
+    //     adaptiveHeight: true,
+        
+    // });
+
+    // $('.team-img-slider').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.team-slider',
+    //     arrows: true,
+    //     centerMode: true,
+    //     adaptiveHeight: true,
+    //     focusOnSelect: true
+    // });
+
     
+
 });
