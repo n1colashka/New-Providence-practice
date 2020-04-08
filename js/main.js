b302aef911c57fed8cd5e47b2e78333e0a84acb3
n1colashka <deckards2505@gmail.com>
2020-04-08 13:10:20 +0600

    header, burger menu finished

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 $(function () {
+    // $('.menu-btn').on('click', function () {
+    //     $('.left-menu').slideToggle();
+    // });
     $('.menu-btn').on('click', function () {
-        $('.left-menu').slideToggle()
+        $('.left-menu').addClass('left-menu--active');
     });
 
     $('.close-btn').on('click', function () {
-        $('.left-menu').slideToggle()
+        // $('.left-menu').slideToggle();
+        $('.left-menu').removeClass('left-menu--active');
+    });
+
+
+    window.addEventListener('scroll', function () {
+        if (this.window.pageYOffset > 0) {
+            $('.header__inner').addClass("header--active");
+        }
+        
+        if (this.window.pageYOffset == 0) {
+            $('.header__inner').removeClass("header--active");
+        }
     });
 
     // $('.team-slider').slick({
@@ -13,7 +28,7 @@ $(function () {
     //     arrows: false,
     //     fade: true,
     //     adaptiveHeight: true,
-        
+
     // });
 
     // $('.team-img-slider').slick({
@@ -26,6 +41,6 @@ $(function () {
     //     focusOnSelect: true
     // });
 
-    
+
 
 });
