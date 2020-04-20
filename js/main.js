27d5aec68a679f23211a7fdaa31b6af4cc06385a
n1colashka <deckards2505@gmail.com>
2020-04-20 11:47:00 +0600

    animation added, icons fixed

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
 $(function () {
-    // $('.menu-btn').on('click', function () {
-    //     $('.left-menu').slideToggle();
-    // });
+
     $('.menu-btn').on('click', function () {
         $('.left-menu').addClass('left-menu--active');
     });
 
     $('.close-btn').on('click', function () {
-        // $('.left-menu').slideToggle();
+
         $('.left-menu').removeClass('left-menu--active');
     });
+
+    
+
+
+    $('.plans__selector-items').on('click', function () {
+
+
+        this.classList.toggle('selector--active');
+        $('.plans__items').toggleClass('items--active');
+
+        $('.plans__items').toggleClass('animated fadeIn slow');
+        
+
+
+    });
+
 
 
     window.addEventListener('scroll', function () {
         if (this.window.pageYOffset > 0) {
             $('.header__inner').addClass("header--active");
         }
-        
+
         if (this.window.pageYOffset == 0) {
             $('.header__inner').removeClass("header--active");
         }
     });
+
+    new WOW().init();
 
     // $('.team-slider').slick({
     //     slidesToShow: 1,
@@ -40,7 +56,6 @@ $(function () {
     //     adaptiveHeight: true,
     //     focusOnSelect: true
     // });
-
 
 
 });
